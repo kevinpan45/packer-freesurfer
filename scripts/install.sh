abed885eeca47ea37b38549141daab17d6e1dca3
KP45 <guohua.pan@neuralgalaxy.com>
2022-01-25 16:53:12 +0800

    update install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,6 +4,6 @@
 apt-get update
 # Install freesurfer
 apt-get install -y -qq bc binutils libgomp1 perl psmisc sudo tar tcsh uuid-dev vim-common libjpeg62-dev libgl-dev wget
-# tar -C /usr/local -xzf /tmp/freesurfer-Linux-centos6_x86_64-stable-pub-v6.0.0.tar.gz
-# chmod a+x -R /usr/local/freesurfer
-# rm /tmp/freesurfer-Linux-centos6_x86_64-stable-pub-v6.0.0.tar.gz
+tar -C /usr/local -xzf /tmp/freesurfer-Linux-centos6_x86_64-stable-pub-v6.0.0.tar.gz
+chmod a+x -R /usr/local/freesurfer
+rm /tmp/freesurfer-Linux-centos6_x86_64-stable-pub-v6.0.0.tar.gz
